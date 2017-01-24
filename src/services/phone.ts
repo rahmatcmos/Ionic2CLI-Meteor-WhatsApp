924afed8475234a16b9027eec20259a08c2aa4be
dotansimha <dotansimha@gmail.com>
2017-01-24 17:22:25 +0200

    Step 7.37: Implement logout method

--- src/services/phone.ts
+++ src/services/phone.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Accounts } from 'meteor/accounts-base';
+import { Meteor } from 'meteor/meteor';
 
 @Injectable()
 export class PhoneService {
@@ -22,6 +23,18 @@ export class PhoneService {
   login(phoneNumber: string, code: string): Promise<void> {
     return new Promise<void>((resolve, reject) => {
       Accounts.verifyPhone(phoneNumber, code, (e: Error) => {
+        if (e) {
+          return reject(e);
+        }
+
+        resolve();
+      });
+    });
+  }
+
+  logout(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      Meteor.logout((e: Error) => {
         if (e) {
           return reject(e);
         }
