1f56a27df5bee2d1e709f9189ba4cf4fa14f84a9
dotansimha <dotansimha@gmail.com>
2017-01-24 10:13:15 +0200

    Step 2.15: Import MomentModule into our app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { ChatsPage } from '../pages/chats/chats';
+import { MomentModule } from 'angular2-moment';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { ChatsPage } from '../pages/chats/chats';
     ChatsPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    MomentModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
