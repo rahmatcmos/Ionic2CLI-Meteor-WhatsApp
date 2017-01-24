fb45d24b3a31204b8593ad91cb257701736876be
dotansimha <dotansimha@gmail.com>
2017-01-24 09:53:11 +0200

    Step 2.5: Add chats page to the NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { ChatsPage } from '../pages/chats/chats';
 
 @NgModule({
   declarations: [
-    MyApp
+    MyApp,
+    ChatsPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp
+    MyApp,
+    ChatsPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
